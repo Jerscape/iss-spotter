aebde2592b16dab81f523f00c84591e698344a6f
Jerscape <jeremydutton5@gmail.com>
2023-07-15 11:41:28 -0400

    IP call implemented

---
+++ iss.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+const fetchMyIp = function(callback){
+  
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+  
+  if (error) {
+    callback(error, null);
+    return;
+  }
+
+  //ADDED FROM COMPASS
+  // if non-200 status, assume server error
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+    return;
+  }
+
+  let data = JSON.parse(body);
+  let ip = data["ip"];
+  callback(null, ip); //I guess we pass null here because the above code already deals with error?
+
+
+  })
+}
+
+module.exports = {
+  fetchMyIp
+}
